8a1fcea0a8c1b924043c0c391004b5635033f712
Zachary Werbalowsky <zachwerbalowsky@Zacharys-MacBook-Pro.local>
2022-01-26 11:13:06 -0800

    Update our database bb

--- W5D3/AA_Questions/import_db.sql
+++ W5D3/AA_Questions/import_db.sql
@@ -1,20 +1,35 @@
+PRAGMA foreign_keys = ON;
+
+DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     id INTEGER PRIMARY KEY,
     fname TEXT NOT NULL,
     lname TEXT NOT NULL
 
 );
-
+DROP TABLE IF EXISTS questions;
 CREATE TABLE questions (
     id INTEGER PRIMARY KEY,
     title TEXT NOT NULL,
     body TEXT NOT NULL,
-    associated_author
-    FOREIGN KEY (associated_author) REFERENCES (users(id))
+    associated_author INTEGER NOT NULL,
+    FOREIGN KEY (associated_author) REFERENCES users(id)
 );
 
+DROP TABLE IF EXISTS question_follows;
 CREATE TABLE question_follows (
-    id INTEGER PRIMARY KEY.
-    users_id
-    questions_id
+    id INTEGER PRIMARY KEY,
+    users_id INTEGER NOT NULL,
+    questions_id INTEGER NOT NULL
 );
+
+INSERT INTO 
+    users(id, fname, lname)
+VALUES 
+    (1, "john", "smith");
+
+INSERT INTO 
+    questions(id, title, body, associated_author)
+VALUES
+    (1, "why?", "Do we ask questions?", 1);
+
