ee33eb264b3d72a645e3c62fdd3de921cb4b6b5f
Jesse-Driskill <jesse@driskill.com>
2022-01-26 10:50:07 -0800

    Adds some tables

--- W5D3/AA_Questions/import_db.sql
+++ W5D3/AA_Questions/import_db.sql
@@ -1,1 +1,20 @@
-sampletext
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
+    fname TEXT NOT NULL,
+    lname TEXT NOT NULL
+
+);
+
+CREATE TABLE questions (
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    body TEXT NOT NULL,
+    associated_author
+    FOREIGN KEY (associated_author) REFERENCES (users(id))
+);
+
+CREATE TABLE question_follows (
+    id INTEGER PRIMARY KEY.
+    users_id
+    questions_id
+);
