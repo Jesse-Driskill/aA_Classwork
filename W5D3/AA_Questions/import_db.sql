2fbb86657724825bce716b7e0fb5cec55f9ad4d9
Jesse-Driskill <jesse@driskill.com>
2022-01-26 11:42:10 -0800

    Adds replies

--- W5D3/AA_Questions/import_db.sql
+++ W5D3/AA_Questions/import_db.sql
@@ -23,6 +23,24 @@ CREATE TABLE question_follows (
     questions_id INTEGER NOT NULL
 );
 
+DROP TABLE IF EXISTS replies;
+CREATE TABLE replies (
+    id INTEGER PRIMARY KEY,
+    subject_question INTEGER NOT NULL, --TEXT, ref to question id, NOT NULL
+    parent_reply INTEGER, -- Refs to previous reply's id
+    question_writer INTEGER NOT NULL,-- user_id, NOT NULL
+    FOREIGN KEY (question_writer) REFERENCES users(id),
+    FOREIGN KEY (subject_question) REFERENCES questions(id),
+    FOREIGN KEY (parent_reply) REFERENCES replies(id)
+);
+
+DROP TABLE IF EXISTS question_likes;
+CREATE TABLE  question_likes (
+    like_question -- Ref to question id
+    like_user -- Ref to user
+    count_likes -- number of users who liked the question
+);
+
 INSERT INTO 
     users(id, fname, lname)
 VALUES 
