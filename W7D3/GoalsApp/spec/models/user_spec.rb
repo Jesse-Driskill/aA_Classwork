de833995346e5c940af2ff655f9143810f424c62
Nick Pietrow <njpietrow@gmail.com>
2022-02-09 14:26:00 -0800

    created user model and tests for validations

--- W7D3/GoalsApp/spec/models/user_spec.rb
+++ W7D3/GoalsApp/spec/models/user_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 require 'rails_helper'
 
 
@@ -10,7 +21,7 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:username) }
     it { should validate_presence_of(:password_digest) }
     it { should validate_presence_of(:session_token) }
-    let(:user) {User.create(username: 'JohnSmith', password: 'password')}
+    subject(:user) {User.create!(username: 'JohnSmith', password: 'password')}
     it { should validate_uniqueness_of(:username) }
     it { should validate_length_of(:password).is_at_least(6) }
   end
