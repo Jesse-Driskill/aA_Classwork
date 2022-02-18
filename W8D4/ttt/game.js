9a02d68b73a342e1f3f14d02bfd453047380d6b0
Jesse-Driskill <jesse@driskill.com>
2022-02-17 16:53:36 -0800

    Adds tictactoe

---
+++ W8D4/ttt/game.js
@@ -0,0 +1,32 @@
+let Board = require("./board");
+
+
+class Game {
+    constructor() {
+
+    }
+
+    run() {
+        
+    }
+}
+
+const reader = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let g = new Game();
+g.run(reader, completion);
+
+function completion() {
+    reader.question("Play again? y or n: ", restartGame => {
+        if (restartGame === "y") {
+            g = new Game();
+            g.run(reader, completion);
+        } else {
+            reader.close();
+        }
+    });
+};
+
