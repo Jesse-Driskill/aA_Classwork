40ea2280899e62680fc509c9056dc95a88b7729d
kingbloopy <kingbloopy@gmail.com>
2022-02-22 16:50:37 -0800

    yay more colors

--- W9D2/BrowserTicTacToe/skeleton/src/ttt-view.js
+++ W9D2/BrowserTicTacToe/skeleton/src/ttt-view.js
@@ -1,6 +1,11 @@
+const Game = require('/ttt_node/game.js');
+
+
 class View {
   constructor(game, el) {
+    this.game = game;
     this.setupBoard();
+    this.el = el;
   }
 
   setupBoard() {
@@ -30,39 +35,45 @@ class View {
     pos9.setAttribute("id","pos9");
     
     let cells = [pos1, pos2, pos3, pos4, pos5, pos6, pos7, pos8, pos9];
-
+    let pos;
+    
     for (let i = 0; i < cells.length; i++) {
-      // cells[i].append("X");
       
       containerUl.appendChild(cells[i]);
-      // containerUl.append(cells[i]);
-      console.log(cells[i]);
+      cells[i].addEventListener('click', () => {
+        if (i === 0) pos = [0,0];
+        if (i === 1) pos = [0,1];
+        if (i === 2) pos = [0,2];
+        if (i === 3) pos = [1,0];
+        if (i === 4) pos = [1,1];
+        if (i === 5) pos = [1,2];
+        if (i === 6) pos = [2,0];
+        if (i === 7) pos = [2,1];
+        if (i === 8) pos = [2,2];
+        if (this.game.board.isEmptyPos(pos)){
+          cells[i].append(this.game.currentPlayer);
+          cells[i].setAttribute('class', `${this.game.currentPlayer}`)
+          this.game.playMove(pos);
+        } else {
+          alert('Move is invalid!');
+        }
+      });
     }
-
-    pos1.append('X');
-    pos7.append('O');
   }
   
-  bindEvents() {
+  // bindEvents() {
+  //   this.handleClick()
+  // }
 
+  // handleClick(e) {
     
-  }
-
-  handleClick(e) {
-    const boxes = document.querySelector("li");
-    boxes.addEventListener('click', e);
-
-  }
-
-  // groceries.addEventListener('click', markAsDone);
-
-  makeMove(square) {
-
-  }
-
+  // }
   
-
   
+  // makeMove(square) {
+    
+  // }
+
 }
 
 
@@ -70,3 +81,24 @@ class View {
 
 module.exports = View;
 
+
+
+
+
+
+
+
+
+
+
+
+// const markAsDone = (e) => {
+//   e.preventDefault();
+
+//   let ele = e.target // the thing that triggered the event/the thing that we are listening for
+
+//   ele.classList.toggle('done') // toggle on or toggle off
+// }
+
+// // add event listener to cross out a list item
+// groceries.addEventListener('click', markAsDone);
