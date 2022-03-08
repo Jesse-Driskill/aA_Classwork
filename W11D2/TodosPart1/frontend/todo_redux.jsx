224e92bd391cbf8dfc5981d3ed673898b24f17f4
Jesse-Driskill <jesse@driskill.com>
2022-03-08 15:12:14 -0800

    Finishes project

--- W11D2/TodosPart1/frontend/todo_redux.jsx
+++ W11D2/TodosPart1/frontend/todo_redux.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from "./store/store"
 import {receiveTodo, receiveTodos, removeTodo} from "./actions/todo_actions"
+import {receiveSteps, receiveStep, removeStep} from "./actions/step_actions"
 
 document.addEventListener("DOMContentLoaded", () => {
     const content = document.getElementById("content")
@@ -11,4 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
     window.receiveTodo = receiveTodo;
     window.receiveTodos = receiveTodos;
     window.removeTodo = removeTodo;
+    window.receiveStep = receiveStep;
+    window.receiveSteps = receiveSteps;
+    window.removeStep = removeStep;
 });
