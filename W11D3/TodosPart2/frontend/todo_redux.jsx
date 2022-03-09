0a23ed682c7617dd4d14182baf2c6223a007d07a
Nick Pietrow <njpietrow@gmail.com>
2022-03-09 12:12:30 -0800

    added part 2 app wrapped in Provider

--- W11D3/TodosPart2/frontend/todo_redux.jsx
+++ W11D3/TodosPart2/frontend/todo_redux.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import configureStore from "./store/store"
+import configureStore from "./store/store";
+import Root from './components/root';
+
 import {receiveTodo, receiveTodos, removeTodo} from "./actions/todo_actions"
 import {receiveSteps, receiveStep, removeStep} from "./actions/step_actions"
 
 document.addEventListener("DOMContentLoaded", () => {
     const content = document.getElementById("content")
-    ReactDOM.render(<h1>ToDos App</h1>, content);
-
-    window.store = configureStore();
+    const store = configureStore();
+    ReactDOM.render(<Root store={store} />, content);
+    
+    window.store = store;
     window.receiveTodo = receiveTodo;
     window.receiveTodos = receiveTodos;
     window.removeTodo = removeTodo;
